fdd57c3ad66900087ce31f5a84634c1a96c945a9
SaToGo <stoledano97@gmail.com>
2023-01-31 01:11:50 +0100

    trying different ways of calling the API

--- src/APIFunctions.js
+++ src/APIFunctions.js
@@ -1,29 +1,18 @@
+
+//same function using async/await and using Promise for training both.
 export async function apiCallAwait(cityName){
     let link = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ea5bc7b59ef81a149e4999e17ee60551`;
     let response = await fetch(link);
-    
-    console.log(response);
-    console.log(response.json());
+    let data = response.json();
+    return data;
 }
-
-
 export function apiCallPromise(cityName){
     let link = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ea5bc7b59ef81a149e4999e17ee60551`;
     console.log(`city name: ${cityName}`)
     console.log(link)
     let data = fetch(link)
-    .then((response) => {
-        console.log('response');
-        console.log(response);
-        console.log('response.json');
-        //console.log(response.json());
-        return response.json();
-    })
-    .then((data) => {
-        console.log('data');
-        console.log(data);
-        return data;
-    })
+    .then((response) => response.json())
+    .then((data) => data );
 
     return data;
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import {apiCallPromise} from './APIFunctions.js'
+import {apiCallPromise, apiCallAwait} from './APIFunctions.js'
 
-let dataP = apiCallPromise('barcelona');
+let dataPromiseBarcelona = apiCallPromise('barcelona');
 
-dataP.then((data) =>{
-    console.log('data outside');
+let data = dataPromiseBarcelona.then((data) =>{
+    console.log('data promise outside');
     console.log(data);
 
     console.log('testing')
     console.log(data.base);
+})
+
+let dataPromiseBarna2 = apiCallAwait('barcelona')
+.then((data)=>{
+    console.log('data await outside');
+    console.log(data);
+
+    console.log('testing await')
+    console.log(data.base);
+
+})
+
+dataAwait.then((data)=>{
+    console.log('last dance testing')
+    console.log(data);
 })

9150c7139133cefc11e278bec9602f00e5b3f623
SaToGo <stoledano97@gmail.com>
2023-02-03 23:30:03 +0100

    add Basic data from the API and Date function

--- src/APIFunctions.js
+++ src/APIFunctions.js
@@ -1,7 +1,14 @@
 
+const UNITS_METRIC = 'metric' 
 //same function using async/await and using Promise for training both.
 export async function apiCallAwait(cityName){
-    let link = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ea5bc7b59ef81a149e4999e17ee60551`;
+    let baseLink = `https://api.openweathermap.org/data/2.5/weather?`;
+    let city = `q=${cityName}`;
+    let key = `appid=ea5bc7b59ef81a149e4999e17ee60551`;
+    let units = `units=${UNITS_METRIC}`;
+
+    let link = baseLink + city + `&` + units + `&` + key;
+
     let response = await fetch(link);
     let data = response.json();
     return data;
--- src/index.js
+++ src/index.js
@@ -2,25 +2,69 @@ import {apiCallPromise, apiCallAwait} from './APIFunctions.js'
 
 let dataPromiseBarcelona = apiCallPromise('barcelona');
 
-let data = dataPromiseBarcelona.then((data) =>{
-    console.log('data promise outside');
-    console.log(data);
+let dataPromiseBarna2 = apiCallAwait('barcelona');
 
-    console.log('testing')
-    console.log(data.base);
-})
+dataPromiseBarna2.then((data)=>{
 
-let dataPromiseBarna2 = apiCallAwait('barcelona')
-.then((data)=>{
-    console.log('data await outside');
-    console.log(data);
+    let date = dateBuilder(data.timezone)
 
-    console.log('testing await')
-    console.log(data.base);
-
-})
-
-dataAwait.then((data)=>{
     console.log('last dance testing')
     console.log(data);
+
+    console.log(`City: ${data.name}`);
+
+    console.log(`Humidity: ${data.main.humidity} %`);
+    console.log(`wind speed: ${data.wind.speed} m/sec`);
+    console.log(`pressure: ${data.main.pressure} hPa`);
+
+    console.log(`temperature: ${data.main.temp} *C` );
+    console.log(`temperature feels like: ${data.main.feels_like} *C` );
+
+    console.log(`clouds: ${data.clouds.all} %`);
+
+    console.log(`timezone: ${date}`);
+
+    console.log(`weather: ${data.weather[0].description}`);
+})
+
+const dateBuilder = (timezone) => {
+   
+    const nowInLocalTime = Date.now()  + 1000 * (timezone / 3600);
+    const millitime = new Date(nowInLocalTime);
+    const dateFormat = millitime.toLocaleString();
+
+    let day = millitime.toLocaleString("en-US", {weekday: "long"});
+    let month = millitime.toLocaleString("en-US", {month: "long"}); 
+    let date = millitime.toLocaleString("en-US", {day: "numeric"});
+    let year = millitime.toLocaleString("en-US", {year: "numeric"}); 
+    let hours = millitime.toLocaleString("en-US", {hour: "numeric"}); 
+    let minutes = millitime.toLocaleString("en-US", {minute: "numeric"});
+
+    return `${day} ${date} ${month} ${year} ${hours}:${minutes}`;
+}
+
+
+let dataPromiseNY = apiCallAwait('new york');
+
+dataPromiseNY.then((data)=>{
+
+    let date = dateBuilder(data.timezone)
+
+    console.log('last dance testing')
+    console.log(data);
+
+    console.log(`City: ${data.name}`);
+
+    console.log(`Humidity: ${data.main.humidity} %`);
+    console.log(`wind speed: ${data.wind.speed} m/sec`);
+    console.log(`pressure: ${data.main.pressure} hPa`);
+
+    console.log(`temperature: ${data.main.temp} *C` );
+    console.log(`temperature feels like: ${data.main.feels_like} *C` );
+
+    console.log(`clouds: ${data.clouds.all} %`);
+
+    console.log(`timezone: ${date}`);
+
+    console.log(`weather: ${data.weather[0].description}`);
 })
